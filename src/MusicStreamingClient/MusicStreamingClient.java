f6e678e727763e51285f4f47af8b0a53105f9acb
thatonedude11 <thatonedude11@gmail.com>
2017-01-18 20:38:17 -0600

    Added basic streaming capability

--- src/MusicStreamingClient/MusicStreamingClient.java
+++ src/MusicStreamingClient/MusicStreamingClient.java
@@ -1,23 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package MusicStreamingClient;
+
+import java.io.*;
+import java.net.*;
+import javax.sound.sampled.*;
 
 /**
  *
- * @author James
+ * @author James Henderson
  */
 public class MusicStreamingClient
 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        // TODO code application logic here
+        System.out.println("Client reading from ");
+        
+        try(Socket socket = new Socket("localhost", 6655))
+        {
+            if(socket.isConnected()) {
+                InputStream in = new BufferedInputStream(socket.getInputStream());
+                play(in);
+            }
+        }
+        
+        System.out.println("Client shutting down");
     }
     
+    private static synchronized void play(final InputStream in) throws Exception
+    {
+        AudioInputStream ais = AudioSystem.getAudioInputStream(in);
+        try(Clip clip = AudioSystem.getClip())
+        {
+            clip.open(ais);
+            clip.start();
+            Thread.sleep(100);
+            clip.drain();
+        }
+    }
 }
